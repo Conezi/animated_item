c68dc1a1e396bb120e7f60e2e8e9b815bb9d14c2
Collins Eneze <eneze.collins@gmail.com>
2023-11-06 00:41:49 +0100

    Implemented all test on animated_item_example.dart

--- example/lib/src/animated_item_example.dart
+++ example/lib/src/animated_item_example.dart
@@ -1,4 +1,5 @@
 import 'package:animated_item/animated_item.dart';
+import 'package:animated_item_example/src/label.dart';
 import 'package:flutter/material.dart';
 
 class AnimatedItemExample extends StatefulWidget {
@@ -9,7 +10,10 @@ class AnimatedItemExample extends StatefulWidget {
 }
 
 class _AnimatedItemExampleState extends State<AnimatedItemExample> {
-  final _scrollController = ScrollController();
+  final _scaleController = ScrollController();
+  final _translateController = ScrollController();
+  final _fadeController = ScrollController();
+  final _rotateController = ScrollController();
   final colors = [
     Colors.pink,
     Colors.yellow,
@@ -30,16 +34,80 @@ class _AnimatedItemExampleState extends State<AnimatedItemExample> {
         body: ListView(
           padding: const EdgeInsets.symmetric(vertical: 15.0),
           children: [
+            const Label('Scale Effect'),
             SizedBox(
               height: 200,
               child: ListView.builder(
                 itemCount: colors.length,
                 scrollDirection: Axis.horizontal,
-                controller: _scrollController,
+                controller: _scaleController,
                 padding: const EdgeInsets.symmetric(horizontal: 10.0),
                 itemBuilder: (context, index) {
                   return AnimatedItem(
-                      controller: _scrollController,
+                      controller: _scaleController,
+                      index: index,
+                      effect: const ScaleEffect(),
+                      child: Container(
+                        margin: const EdgeInsets.all(5.0),
+                        width: width,
+                        color: colors[index],
+                      ));
+                },
+              ),
+            ),
+            const Label('Translate Effect'),
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                itemCount: colors.length,
+                scrollDirection: Axis.horizontal,
+                controller: _translateController,
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                itemBuilder: (context, index) {
+                  return AnimatedItem(
+                      controller: _translateController,
+                      index: index,
+                      effect: const TranslateEffect(animationAxis: Axis.vertical, start: 5),
+                      child: Container(
+                        margin: const EdgeInsets.all(5.0),
+                        width: width,
+                        color: colors[index],
+                      ));
+                },
+              ),
+            ),
+            const Label('Fade Effect'),
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                itemCount: colors.length,
+                scrollDirection: Axis.horizontal,
+                controller: _fadeController,
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                itemBuilder: (context, index) {
+                  return AnimatedItem(
+                      controller: _fadeController,
+                      index: index,
+                      effect: const FadeEffect(),
+                      child: Container(
+                        margin: const EdgeInsets.all(5.0),
+                        width: width,
+                        color: colors[index],
+                      ));
+                },
+              ),
+            ),
+            const Label('Fade Effect'),
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                itemCount: colors.length,
+                scrollDirection: Axis.horizontal,
+                controller: _rotateController,
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                itemBuilder: (context, index) {
+                  return AnimatedItem(
+                      controller: _rotateController,
                       index: index,
                       effect: const RotateEffect(),
                       child: Container(
---
+++ example/lib/src/label.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Label extends StatelessWidget {
+  final String label;
+  const Label(this.label, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0),
+        child: Text(label,
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600)),
+      ),
+    );
+  }
+}
